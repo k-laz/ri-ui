9d2d555d426e157738ada8ce4c4768634ad759b2
Kirill Lazarev <klazarev2000@gmail.com>
2024-10-01 21:03:24 -0700

    fix filter

--- src/pages/Filter.tsx
+++ src/pages/Filter.tsx
@@ -103,18 +103,18 @@ const Filter = () => {
                           className="custom-range-slider w-2/3 appearance-none sm:w-full"
                           style={{
                             '--slider-value': `${
-                              (values.price_limit / 5000) * 100
+                              ((values.price_limit ?? 0) / 5000) * 100
                             }%`,
                           }}
-                          value={values.price_limit}
+                          value={values.price_limit ?? 0}
                           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                             setFieldValue(
                               'price_limit',
-                              e.target.value ? parseInt(e.target.value) : '',
+                              e.target.value ? parseInt(e.target.value) : 0,
                             )
                           }
                         />
-                        {/* Number Input Field with + sign */}
+                        {/* Disabled Number Input Field */}
                         <div className="relative">
                           <Field
                             id="price_limit_input"
@@ -123,20 +123,13 @@ const Filter = () => {
                             min="0"
                             max="5000"
                             step="10"
-                            className="ml-4 min-w-20 truncate rounded-md border-2 border-primary p-1 text-center lg:pl-4" /* Added padding-right to create space for + sign */
-                            value={values.price_limit}
-                            onChange={(
-                              e: React.ChangeEvent<HTMLInputElement>,
-                            ) =>
-                              setFieldValue(
-                                'price_limit',
-                                e.target.value ? parseInt(e.target.value) : '',
-                              )
-                            }
+                            className="ml-4 min-w-20 truncate rounded-md border-2 border-primary p-1 text-center lg:pl-4"
+                            value={values.price_limit ?? 0}
+                            disabled
                           />
                           {/* Conditionally display + sign inside the input field */}
-                          {values.price_limit >= 5000 && (
-                            <span className="absolute inset-y-0 right-2 flex items-center pr-4">
+                          {(values.price_limit ?? 0) >= 5000 && (
+                            <span className="absolute inset-y-0 right-12 flex items-center pr-4">
                               +
                             </span>
                           )}
